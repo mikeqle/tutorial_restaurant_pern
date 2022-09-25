12cc01fd5e59b94bca5e601f3ef078820069f9db
Mike Le <mike.le@live.com>
2022-09-25 16:53:52 +0700

    reorder lines in server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
-
 const express = require('express');
-
-// Default port = 8001 if no environment var PORT found
-const port = process.env.PORT || 8001;
 
 const app = express();
 
+// Default port = 8001 if no environment var PORT found
+const port = process.env.PORT || 8001;
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
