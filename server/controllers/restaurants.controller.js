978efdeb4f33c5c23b2d6706b2c80cd050feb7c3
Mike Le <mike.le@live.com>
2022-09-26 11:37:31 +0700

    finished backend api with 5 endpoints

--- server/controllers/restaurants.controller.js
+++ server/controllers/restaurants.controller.js
@@ -3,50 +3,90 @@ const db = require('../db');
 module.exports ={
     getAllRestaurants: async (req, res) => {
         console.log("Getting All Restaurants");
-        const results = await db.query("SELECT * FROM restaurants;");
-        console.log(results);
-        res.status(200).json({
-            status: "success",
-            data: {
-                restaurant: ['mcdonalds', 'wendys']
-            }
-        });
+        try {
+            const results = await db.query("SELECT * FROM restaurants;");
+            // console.log(results);
+            res.status(200).json({
+                status: "success",
+                results: results.rows.length,
+                data: {
+                    restaurant: results.rows
+                }
+            });    
+        } catch (err) {
+            console.log(err);
+        };
     },
 
-    getOneRestaurant: (req, res) => {
+    getOneRestaurant: async (req, res) => {
         console.log("Getting One Restaurants");
-        res.status(200).json({
-            status: "success",
-            data: {
-                restaurant: "McDonald's"
-            }
-        });
+        try {
+            const results = await db.query(
+                "SELECT * FROM restaurants WHERE id = $1;",
+                [req.params.id]
+            );
+            res.status(200).json({
+                status: "success",
+                data: {
+                    restaurant: results.rows[0]
+                }
+            })
+        } catch (err) {
+            console.log(err);
+        };
     },
 
-    createRestaurant: (req, res) => {
+    createRestaurant: async (req, res) => {
         console.log("Creating Restaurant");
-        res.status(201).json({
-            status: "success",
-            data: {
-                restaurant: "McDonald's"
-            }
-        });
+        try {
+            const results = await db.query(
+                "INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *;",
+                [req.body.name, req.body.location, req.body.price_range]
+            );
+            console.log("Restaurant successfully created.");
+            res.status(201).json({
+                status: "success",
+                data: {
+                    restaurant: results.rows[0],
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        };
     },
 
-    updateRestaurant: (req, res) => {
+    updateRestaurant: async (req, res) => {
         console.log("Updating Restaurant");
-        res.status(200).json({
-            status: "success",
-            data: {
-                restaurant: "McDonald's"
-            }
-        });
+        try {
+            const results = await db.query(
+                "UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *",
+                [req.body.name, req.body.location, req.body.price_range, req.params.id]
+            );
+            res.status(200).json({
+                status: "success",
+                data: {
+                    restaurant: results.rows[0]
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        };
     },
 
-    deleteRestaurant: (req, res) => {
+    deleteRestaurant: async (req, res) => {
         console.log("Deleting Restaurant");
-        res.status(204).json({
-            status: "success"
-        })
+        try {
+            const results = await db.query(
+                "DELETE FROM restaurants WHERE id = $1 RETURNING *",
+                [req.params.id]
+            );
+            
+            res.status(200).json({
+                status: "success",
+                restaurant: results.rows[0]
+            });    
+        } catch (err) {
+            console.log(err);
+        };
     },
 }
