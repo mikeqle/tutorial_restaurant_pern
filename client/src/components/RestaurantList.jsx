eb184f0e3acc345e6699c0c0e4deb1a4fd79845d
Mike Le <mike.le@live.com>
2022-09-27 11:55:35 +0700

    delete button implemented and tested

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -17,6 +17,17 @@ const RestaurantList = () => {
         fetchData();
     }, [])
 
+    const handleDelete = async (id) => {
+        try {
+            const response = await RestaurantFinder.delete(`/${id}`);
+            setRestaurants(restaurants.filter((restaurant) => {
+                return restaurant.id !== id;
+            }))
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
   return (
     <div className="list-group">
         <table className="table table-hover table-dark">
@@ -39,7 +50,7 @@ const RestaurantList = () => {
                         <td>{"$".repeat(restaurant.price_range)}</td>
                         <td>reviews</td>
                         <td><button className="btn btn-warning">Update</button></td>
-                        <td><button className="btn btn-danger">Delete</button></td>
+                        <td><button onClick={() => handleDelete(restaurant.id)} className="btn btn-danger">Delete</button></td>
                     </tr>
                     );
                 })}
