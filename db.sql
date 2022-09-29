1bc60ea239da9a8220f8672efacb52721e2e0a85
Mike Le <mike.le@live.com>
2022-09-29 16:30:41 +0700

    created reviews table

--- db.sql
+++ db.sql
@@ -10,3 +10,13 @@ CREATE TABLE restaurants (
     location VARCHAR(200) NOT NULL,
     price_range INT NOT NULL CHECK(price_range >= 1 AND price_range <= 5)
 );
+
+
+-- Create table reviews
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(200) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL CHECK(rating >= 1 AND rating <= 5)
+);
