4d7fa2d34b4c96f8b22e1c34986327efb2b71775
Mike Le <mike.le@live.com>
2022-10-03 17:42:47 +0700

    edit api path for production deployment

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+// NODE_ENV
+
+const baseURL = 
+    process.env.NODE_ENV === 'production' 
+        ? "api/v1/restaurants" 
+        : "http://localhost:8000/api/v1/restaurants";
+ 
+
 export default axios.create({
-    baseURL: "http://localhost:8000/api/v1/restaurants"
-})
+    baseURL,
+});
