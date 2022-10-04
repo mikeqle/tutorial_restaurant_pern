38d2e2b52750c61043c04377b4b44ac4bfb5a9c8
Mike Le <mike.le@live.com>
2022-10-04 10:49:45 +0700

    fixing get request issue with getOneRestaurant on production

--- server/server.js
+++ server/server.js
@@ -11,6 +11,17 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
+
+// Handling issues with GET request for getOneRestaurant
+app.use(express.static(path.join(__dirname, 'build')));
+
+// PATH CONFIGURATION TO RESPOND TO A REQUEST TO STATIC ROUTE REQUEST BY SERVING index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+
 // Routes
 require('./routes/restaurants.routes')(app);
 
